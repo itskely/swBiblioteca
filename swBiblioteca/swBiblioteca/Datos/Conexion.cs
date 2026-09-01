29ec1dd25d6ead8ef1a564e23ffd695525c55a89
kely yohana Quilindo Hernández <quilindok88@gmail.com>
2026-08-31 19:40:12 -0500

    Conexion a base de datos

---
+++ swBiblioteca/swBiblioteca/Datos/Conexion.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace SwBiblioteca.Datos
+{
+    public class Conexion
+    {
+        private string cadenaConexion =
+            @"Server=.\DESARROLLO;Database=Biblioteca;Integrated Security=True;TrustServerCertificate=True;";
+
+        public SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(cadenaConexion);
+        }
+    }
+}
